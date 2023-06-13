c2001b64b66c357f959369b6f890c71ca901302c
euqirn3h <henrique11rodrigues@gmail.com>
2023-06-13 11:30:52 -0300

    Adding a Middlewares examples

---
+++ Examples/Middlewares/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const { logger } = require('./middlewares/LogEvents');
+const { errorHandler } = require('./middlewares/errorHandler');
+const cors = require('cors');
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+//Middlewares
+
+//'content-type': application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// Built-in middleware to deal with Json
+app.use(express.json());
+
+// Built-in middleware to deal with static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+//Custom middleware
+app.use(logger);
+
+//Dependency middleware to cross origin resourrce sharing
+const domainList = ['http://localhost:3500', undefined  ]
+const corsOption = {
+    origin: (origin, callback) => {
+        if(domainList.indexOf(origin) !== -1){
+            callback(null, true);
+        }else{
+            callback(new Error('Not allowed by CORS !!'));
+        }
+    },
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOption));  
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('^/$|/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html');
+});
+
+// 404 route should be on end of routes
+app.all('*', (req, res) => {
+    res.status(404);
+    if( req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    }else if(req.accepts('json')){
+        res.json({ error: "404 Not Found"});
+    }else{
+        res.type('txt').send("404 Not Found");
+    }
+    
+});
+
+//Custom middleware to handle errors
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}!`))
+
